8bf0096c774463d3b72026d75ac877c4c37a2cd2
Harshit Shukla <harshitshukla258@gmail.com>
2022-06-17 10:16:47 +0530

    delte and edit button added

--- src/components/TableCreate/TableTabs.js
+++ src/components/TableCreate/TableTabs.js
@@ -6,7 +6,8 @@ import Editor from '../Editor/EditorMain';
 import AddBoxIcon from '@mui/icons-material/AddBox';
 import './styles.css';
 import IconButton from "@mui/material/IconButton";
-
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export default function TableTabs() {
   const [arrHead, setArrHead] = React.useState(["Tab 1"]);
@@ -25,7 +26,40 @@ export default function TableTabs() {
         return [...prev, false]
     })
   }
+  const editTabFunc = (index) => {
+    const prompt = window.prompt("Enter new tab name");
+    if(prompt){
+      if(prompt.length >= 4){
+        alert("Tab name must be less than or equal to 4 characters");
+      }
+      else{
+       arrHead[index] = prompt;
+       setArrHead([arrHead]);
+      }
+    }
+  }
+  const refresfDataFunc = (index) => {
+    arrResult[index] = false;
+    setArrResult(arrResult);
+    arrBody[index] = "";
+    setArrBody(arrBody);
+  }
+  const deleteTabFunc = (index) => {
+    if(arrHead.length == 1){
+      alert("You can't delete the last tab");
+    }
+    else{
+    
+    refresfDataFunc(index);
 
+    arrBody.splice(index, 1);
+    arrResult.splice(index, 1);
+    arrHead.splice(index, 1);
+    
+    setArrBody([...arrBody]);
+    setArrResult([...arrResult]);}
+    setArrHead([...arrHead]);
+  }
   return (
     <div className="App">
       <Tabs>
@@ -33,7 +67,16 @@ export default function TableTabs() {
           <TabList>
           {
               arrHead.map((val,index) => {
-                  return <Tab><p >{val}</p></Tab>
+                  return <Tab>
+                  <p >{val}
+                  <IconButton >
+                    <EditIcon onClick={() => editTabFunc(index)}/>
+                  </IconButton>
+                  <IconButton>
+                    <DeleteIcon onClick={() => deleteTabFunc(index) }/>
+                  </IconButton>
+                  </p>
+                  </Tab>
               })
               
           }
@@ -56,6 +99,7 @@ export default function TableTabs() {
                           setArrBody={setArrBody} 
                           setArrResult = {setArrResult}
                           arrResult={arrResult}
+                          arrHead={arrHead}
                         />
                     </div>
                 </TabPanel>
